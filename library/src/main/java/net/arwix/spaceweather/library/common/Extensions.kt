83984f3cd69a9e2aced917815a447603868d47b3
Vitaliy Sheyanov <vit.onix@gmail.com>
2020-06-06 00:20:44 +0300

    add proton chunk

--- library/src/main/java/net/arwix/spaceweather/library/common/Extensions.kt
+++ library/src/main/java/net/arwix/spaceweather/library/common/Extensions.kt
@@ -1,15 +1,27 @@
 package net.arwix.spaceweather.library.common
 
 import net.arwix.spaceweather.library.geomagnetic.data.KpIndexData
+import net.arwix.spaceweather.library.radiation.data.ProtonData
 
 /**
  * reversed list
  */
-fun List<KpIndexData>.chunkToBar(): List<KpIndexData> {
+fun List<KpIndexData>.chunkKpIndexToBar(size: Int = 36): List<KpIndexData> {
     val innerData = dropWhile { it.time % 10800 != 0L }
-    val groups = innerData.chunked(36)
+    val groups = innerData.chunked(size)
     return groups.asSequence().map { item ->
         KpIndexData(
+            time = item.first().time,
+            value = item.maxBy { it.value }!!.value
+        )
+    }.toList()
+}
+
+fun List<ProtonData>.chunkProtonDataToBar(size: Int = 36): List<ProtonData> {
+    val innerData = dropWhile { it.time % 10800 != 0L }
+    val groups = innerData.chunked(size)
+    return groups.asSequence().map { item ->
+        ProtonData(
             time = item.first().time,
             value = item.maxBy { it.value }!!.value
         )
