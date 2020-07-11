24d4b0b4c88e5be1242a6c97e8988a8b814f33b8
Vitaliy Sheyanov <vit.onix@gmail.com>
2020-07-11 20:57:29 +0300

    add alert checker

---
+++ library/src/main/java/net/arwix/spaceweather/library/domain/WeatherAlertChecker.kt
@@ -0,0 +1,52 @@
+package net.arwix.spaceweather.library.domain
+
+import net.arwix.spaceweather.library.data.WeatherSWPCData
+
+abstract class WeatherAlertChecker<T: WeatherSWPCData> {
+
+    protected abstract fun saveCurrentAlert(data: T)
+    protected abstract fun getPreviousAlert(): T?
+    protected abstract fun copyData(data: T, time: Long): T?
+    protected abstract fun alert(data: T)
+
+    /**
+     * @param minAlertIndex
+     * @param dataArray [0] current... [3] older
+     * @param minDeltaTime is seconds
+     */
+    open fun check(
+        minAlertIndex: Int,
+        dataArray: Array<T>,
+        minDeltaTime: Long
+    ) {
+        val maxElement = dataArray.maxBy { it.getIntIndex() }!!
+        if (maxElement.getIntIndex() < minAlertIndex) return
+
+        val (currentData, old3Data, old6Data) = dataArray
+//        val old6Index = old6Data.getIntIndex()
+        val old3Index = old3Data.getIntIndex()
+        val currentIndex = currentData.getIntIndex()
+
+        val previousAlertData = getPreviousAlert()
+        if ((previousAlertData?.time ?: -1) < old6Data.time) {
+            alert(maxElement)
+            saveCurrentAlert(maxElement)
+        } else if (previousAlertData != null) {
+            if (maxElement.getIntIndex() > previousAlertData.getIntIndex()) {
+                // 4 5 6
+                alert(maxElement)
+                saveCurrentAlert(maxElement)
+            } else if (
+                currentIndex > minAlertIndex &&
+                old3Index < currentIndex &&
+                currentData.time > previousAlertData.time + minDeltaTime
+            ) {
+                // 4 6 5
+                alert(currentData)
+                copyData(previousAlertData, maxElement.time)
+                copyData(previousAlertData, maxElement.time)?.let(::saveCurrentAlert)
+            }
+        }
+    }
+
+}
