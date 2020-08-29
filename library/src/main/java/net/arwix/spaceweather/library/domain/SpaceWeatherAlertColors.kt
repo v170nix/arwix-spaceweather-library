d6db8a81d927c34ffa4adb85419d3f51ac0cd1b0
Vitaliy Sheyanov <vit.onix@gmail.com>
2020-08-29 22:13:46 +0300

    add notification

---
+++ library/src/main/java/net/arwix/spaceweather/library/domain/SpaceWeatherAlertColors.kt
@@ -0,0 +1,135 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
+package net.arwix.spaceweather.library.domain
+
+import androidx.annotation.ColorInt
+
+abstract class SpaceWeatherAlertColors {
+
+    enum class TypeColor { Quiet, Active, Minor, Major, Extreme }
+
+    protected abstract val array: Array<Int>
+
+    @ColorInt
+    open fun getGeomagneticColor(index: Int): Int {
+        return when (index) {
+            4 -> this[TypeColor.Active]
+            5 -> this[TypeColor.Minor]
+            in 6..8 -> this[TypeColor.Major]
+            9 -> this[TypeColor.Extreme]
+            else -> this[TypeColor.Quiet]
+        }
+    }
+
+    @ColorInt
+    open fun getHemisphericColor(value: Int) = when (value) {
+        in 20..50 -> this[TypeColor.Active]
+        in 50..80 -> this[TypeColor.Minor]
+        in 80..110 -> this[TypeColor.Minor]
+        in 110..Int.MAX_VALUE -> this[TypeColor.Extreme]
+        else -> this[TypeColor.Quiet]
+    }
+
+    @ColorInt
+    open fun getSolarWindSpeedColor(speed: Double): Int =
+        this[TypeColor.values()[getSolarWindSpeedIndex(speed)]]
+
+    open fun getSolarWindSpeedIndex(speed: Double): Int = when (speed) {
+        in 400.0..500.0 -> 1
+        in 500.0..700.0 -> 2
+        in 700.0..900.0 -> 3
+        in 900.0..Double.POSITIVE_INFINITY -> 4
+        else -> 0
+    }
+
+    @ColorInt
+    open fun getSolarWindDensityColor(density: Double): Int =
+        this[TypeColor.values()[getSolarWindDensityIndex(density)]]
+
+    open fun getSolarWindDensityIndex(density: Double): Int = when (density) {
+        in 10.0..20.0 -> 1
+        in 20.0..40.0 -> 2
+        in 40.0..60.0 -> 3
+        in 60.0..Double.POSITIVE_INFINITY -> 4
+        else -> 0
+    }
+
+    @ColorInt
+    open fun getSolarWindTemperatureColor(temperature: Double): Int =
+        this[TypeColor.values()[getSolarWindTemperatureIndex(temperature)]]
+
+    open fun getSolarWindTemperatureIndex(temperature: Double): Int = when (temperature) {
+        in 30000.0..300000.0 -> 1
+        in 300000.0..3000000.0 -> 2
+        in 3000000.0..Double.POSITIVE_INFINITY -> 3
+        else -> 0
+    }
+
+    @ColorInt
+    open fun getSolarWindBtColor(bt: Double): Int =
+        this[TypeColor.values()[getSolarWindBtIndex(bt)]]
+
+    open fun getSolarWindBtIndex(bt: Double): Int = when (bt) {
+        in 5.0..10.0 -> 1
+        in 10.0..20.0 -> 2
+        in 20.0..30.0 -> 3
+        in 30.0..Double.POSITIVE_INFINITY -> 4
+        else -> 0
+    }
+
+    @ColorInt
+    open fun getSolarWindBzColor(bz: Double): Int =
+        this[TypeColor.values()[getSolarWindBzIndex(bz)]]
+
+    open fun getSolarWindBzIndex(bz: Double): Int = when (bz) {
+        in Double.NEGATIVE_INFINITY..-20.0 -> 4
+        in -20.0..-10.0 -> 3
+        in -10.0..-5.0 -> 2
+        in -5.0..0.0 -> 1
+        else -> 0
+    }
+
+    @ColorInt
+    open fun getRadiationColor(index: Int): Int {
+        return when (index) {
+            1 -> this[TypeColor.Minor]
+            2 -> this[TypeColor.Major]
+            3 -> this[TypeColor.Major]
+            4 -> this[TypeColor.Extreme]
+            5 -> this[TypeColor.Extreme]
+            else -> this[TypeColor.Quiet]
+        }
+    }
+
+    @ColorInt
+    open fun getXRayColor(index: Int): Int {
+        return when (index) {
+            1 -> this[TypeColor.Minor]
+            2 -> this[TypeColor.Major]
+            3 -> this[TypeColor.Major]
+            4 -> this[TypeColor.Extreme]
+            5 -> this[TypeColor.Extreme]
+            else -> this[TypeColor.Quiet]
+        }
+    }
+
+    @ColorInt
+    open fun getFlareColor(flareClass: String): Int {
+        val mainClass = flareClass.first().toString()
+        val value = flareClass.substring(1).toDoubleOrNull() ?: 0.0
+        return when (mainClass) {
+            "M" -> if (value < 5.0) this[TypeColor.Minor] else this[TypeColor.Major]
+            "X" -> if (value < 10.0) this[TypeColor.Major] else this[TypeColor.Extreme]
+            else -> this[TypeColor.Quiet]
+        }
+    }
+
+    @ColorInt
+    operator fun get(typeColor: TypeColor): Int = when (typeColor) {
+        TypeColor.Quiet -> array[0]
+        TypeColor.Active -> array[1]
+        TypeColor.Minor -> array[2]
+        TypeColor.Major -> array[3]
+        TypeColor.Extreme -> array[4]
+    }
+}
---
+++ library/src/main/java/net/arwix/spaceweather/library/domain/SpaceWeatherNotificationManager.kt
@@ -0,0 +1,205 @@
+@file:Suppress("unused")
+
+package net.arwix.spaceweather.library.domain
+
+import android.app.Activity
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import androidx.annotation.ColorInt
+import androidx.core.app.NotificationManagerCompat
+import net.arwix.extension.getTextRect
+import net.arwix.notification.createNotificationChannels
+import net.arwix.notification.notification
+import net.arwix.spaceweather.library.R
+import net.arwix.spaceweather.library.geomagnetic.data.KpIndexData
+import net.arwix.spaceweather.library.radiation.data.ProtonData
+import net.arwix.spaceweather.library.xray.data.XRayFlareEventData
+
+class SpaceWeatherNotificationManager(
+    private val applicationContext: Context,
+    private val activityClass: Class<out Activity>,
+    private val config: Config
+) {
+
+    data class Config(
+        val channelId: String = "SpaceWeather.notification.activity.id",
+        val geomagneticIdAction: IdAction,
+        val radiationIdAction: IdAction,
+        val flareIdAction: IdAction
+    ) {
+        data class IdAction(val id: Int, val action: String)
+    }
+
+    init {
+        createNotificationChannels(applicationContext) {
+            channel(
+                config.channelId,
+                applicationContext.getString(R.string.space_weather_notification_channel_solar_activity),
+                importance = NotificationManagerCompat.IMPORTANCE_LOW
+            ) {
+                vibrationEnabled = false
+            }
+        }
+    }
+
+    fun doGeomagneticNotify(kpIndexData: KpIndexData, alertColorsSpace: SpaceWeatherAlertColors) {
+        getManager()?.notify(config.geomagneticIdAction.id,
+            notification(applicationContext, config.channelId, R.drawable.ic_geo) {
+
+                val index = kpIndexData.getIntIndex().takeIf { it > 3 } ?: return
+                val largeIcon = createNotificationIcon(
+                    alertColorsSpace.getGeomagneticColor(index),
+                    if (index > 4) "G${index - 4}" else "A"
+                )
+
+                contentTitle(applicationContext.getString(R.string.space_weather_notification_geomagnetic_header))
+                subText(if (index > 4) "G${index - 4}" else "A")
+                contentText(applicationContext.resources.getStringArray(R.array.space_weather_notification_geomagnetic_info)[index - 4])
+                whenTime(kpIndexData.time * 1000L)
+                largeIcon(largeIcon)
+                contentIntent(createIntent(config.geomagneticIdAction.action))
+                autoCancel(true)
+            }
+        )
+    }
+
+    fun doProtonNotify(protonData: ProtonData, alertColorsSpace: SpaceWeatherAlertColors) {
+        getManager()?.notify(config.radiationIdAction.id,
+            notification(
+                applicationContext,
+                config.channelId,
+                R.drawable.ic_solar_radiation
+            ) {
+
+                val index = protonData.getIntIndex()
+                val largeIcon = createNotificationIcon(
+                    alertColorsSpace.getRadiationColor(index),
+                    "S$index"
+                )
+
+                contentTitle(applicationContext.getString(R.string.space_weather_notification_geomagnetic_header))
+                subText("S$index")
+                contentText(applicationContext.resources.getStringArray(R.array.space_weather_notification_radiation_info)[index])
+                whenTime(protonData.time * 1000L)
+                largeIcon(largeIcon)
+                contentIntent(createIntent(config.radiationIdAction.action))
+                autoCancel(true)
+            }
+        )
+    }
+
+    fun doFlareNotify(flareData: XRayFlareEventData, alertColorsSpace: SpaceWeatherAlertColors) {
+        getManager()?.notify(config.flareIdAction.id,
+            notification(
+                applicationContext,
+                config.channelId,
+                R.drawable.ic_solar_flare
+            ) {
+                contentTitle(applicationContext.getString(R.string.space_weather_notification_solar_flare_header))
+
+                val eventClass = flareData.maxClass.orEmpty().trim()
+                val isMaxEvent = eventClass.isNotEmpty()
+
+                if (isMaxEvent) {
+                    val maxValue = eventClass.substring(1).toDoubleOrNull() ?: 0.0
+                    val index = when (eventClass) {
+                        "M" -> if (maxValue < 5.0) 0 else 1
+                        "X" -> if (maxValue < 10.0) 2 else if (maxValue < 20.0) 3 else 4
+                        else -> 0
+                    }
+                    subText(eventClass)
+                    contentText(
+                        applicationContext.getString(
+                            R.string.space_weather_notification_solar_flare_context,
+                            eventClass
+                        )
+                    )
+                    whenTime(flareData.maxTime ?: 0L)
+                    largeIcon(
+                        createNotificationIcon(
+                            alertColorsSpace.getFlareColor(eventClass),
+                            "R$index"
+                        )
+                    )
+                } else {
+                    contentText(applicationContext.getString(R.string.space_weather_notification_solar_flare_begin_event_context))
+                    whenTime(flareData.beginTime)
+                    largeIcon(
+                        createNotificationIcon(
+                            alertColorsSpace[SpaceWeatherAlertColors.TypeColor.Active],
+                            " "
+                        )
+                    )
+                }
+                contentIntent(createIntent(config.flareIdAction.action))
+                autoCancel(true)
+            }
+        )
+    }
+
+    private fun getManager() =
+        applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+
+
+    private fun createIntent(keyAction: String) =
+        PendingIntent.getActivity(
+            applicationContext, 0,
+            Intent(applicationContext, activityClass).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                        Intent.FLAG_ACTIVITY_NO_ANIMATION
+                action = keyAction
+            },
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+    private fun createNotificationIcon(@ColorInt color: Int, text: String): Bitmap {
+        val size = (24f * applicationContext.resources.displayMetrics.density).toInt()
+        val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.SUBPIXEL_TEXT_FLAG)
+        val arcPaint = Paint(paint)
+        arcPaint.color = color
+        arcPaint.alpha = (0.7 * 255).toInt()
+        canvas.drawCircle(size / 2f, size / 2f, size / 2f, arcPaint)
+        arcPaint.alpha = 255
+        canvas.drawCircle(
+            size / 2f,
+            size / 2f,
+            size / 2f - 2f * applicationContext.resources.displayMetrics.density,
+            arcPaint
+        )
+
+        paint.textSize = bitmap.width / 2.5f
+        paint.isFakeBoldText = true
+        paint.color = Color.WHITE
+        val bounds = paint.getTextRect(text)
+        val widthArray = FloatArray(2)
+        paint.getTextWidths(text.first().toString(), widthArray)
+        paint.textAlign = Paint.Align.CENTER
+        canvas.drawText(
+            text,
+            bitmap.width / 2f,
+            bitmap.height / 2f + bounds.height() / 2f,
+            paint
+        )
+        return bitmap
+    }
+
+    private companion object {
+//        private const val NOTIFICATION_CHANNEL_ACTIVITY_ID = "SpaceWeather.notification.activity.id"
+//        const val NOTIFICATION_GEOMAGNETIC_ID = 484
+//        const val NOTIFICATION_RADIATION_ID = 485
+//        const val NOTIFICATION_RADIO_ID = 486
+//        const val INTENT_GEOMAGNETIC_KEY_ACTION = "SpaceWeather.notification.intent.geomagnetic"
+//        const val INTENT_RADIATION_KEY_ACTION = "SpaceWeather.notification.intent.radiation"
+//        const val INTENT_X_RAY_KEY_ACTION = "SpaceWeather.notification.intent.x_ray.Notification"
+    }
+
+}
